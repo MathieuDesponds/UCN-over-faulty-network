c47e6b801d1ea9d7ef345b7892c299863b786beb
Mathieu Desponds <mathieu.desponds@epfl.ch>
2021-10-12 11:38:00 +0200

    First version of TCP
    Sending :
    - send the list of messages using udp
    - listen to port for ack message
    - remove the message from the list using hashmaps
    - resend what was not receive after a timeout
    
    Receiving :
    - send a ack with the seq number

--- template_java/src/main/java/cs451/links/PerfectLink.java
+++ template_java/src/main/java/cs451/links/PerfectLink.java
@@ -1,4 +1,49 @@
 package cs451.links;
 
-public class PerfectLink {
+import cs451.Message;
+
+import java.lang.reflect.Array;
+import java.net.SocketTimeoutException;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class PerfectLink extends Link{
+    private FairLossLink fll;
+    private int waitingFor = 0; //for receiver
+    private final int TIMEOUT = 8000;
+
+    public PerfectLink(String ip, int port, String output){
+        fll = new FairLossLink(ip,port,TIMEOUT, output);
+    }
+    @Override
+    public Message deliver() throws SocketTimeoutException {
+        Message m = fll.deliver();
+        Message ack = new Message(m.getSrcIP(), m.getSrcPort(), m.getSeqNumber(),"");
+        fll.send(List.of(ack));
+        return m;
+    }
+
+    @Override
+    public void send(List<Message> lm) {
+        fll.send(lm);
+        HashSet<Integer> hs = new HashSet<Integer>(lm.stream().map(m -> m.getSeqNumber()).collect(Collectors.toSet()));
+        Message m;
+        try{
+            while(true){
+                m = fll.deliver();
+                hs.remove(m.getSeqNumber());
+            }
+        }catch(SocketTimeoutException e) {
+            System.out.println("Timeout");
+            send(hs.stream().map(i -> lm.get(i)).collect(Collectors.toList()));
+        }
+    }
+
+    @Override
+    public void close(){
+        fll.close();
+    }
 }
