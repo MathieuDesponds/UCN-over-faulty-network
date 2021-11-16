90f9745815db2a9bf37e8d6beb4a0b6a5cec1bf7
Mathieu Desponds <mathieu.desponds@epfl.ch>
2021-11-16 09:51:32 +0100

    Add performance count in OutputLayer

--- 283229/src/main/java/cs451/layers/OutputLayer.java
+++ 283229/src/main/java/cs451/layers/OutputLayer.java
@@ -14,18 +14,29 @@ public class OutputLayer extends Layer{
     private String path;
     private boolean closed = false;
 
+    //For performance
+    private int numberOfTotalMessage;
+    private long startTime = 0;
+    private long finishedTime = 0;
+
     public OutputLayer(Layer topLayer, Parser parser){
         this.path = parser.output();
         output = new ArrayList<>();
         Layer downLayer = new FIFOUniformBroadcast(this, parser);
         super.setDownLayer(downLayer);
         super.setTopLayer(topLayer);
+
+        int nbM = parser.configNbMessage();
+        numberOfTotalMessage = nbM * (parser.NUMBER_OF_HOSTS+1);
     }
 
     @Override
     public <BM extends Message> void deliveredFromBottom(BM m) {
         if(!closed) {
             output.add("d " + ((BroadcastMessage)m).getBroadcasterID() + " " + m.getSeqNumber());
+        }
+        if(output.size() == numberOfTotalMessage){
+            finishedTime = System.currentTimeMillis();
         }
     }
 
@@ -35,6 +46,8 @@ public class OutputLayer extends Layer{
             downLayer.sendFromTop(m);
             output.add("b " + m.getSeqNumber());
         }
+        if(startTime==0)
+            startTime = System.currentTimeMillis();
     }
 
     @Override
@@ -49,9 +62,11 @@ public class OutputLayer extends Layer{
             for(String s : output){
                 writer.write(s+"\n");
             }
+            writer.write("Time is "+(finishedTime-startTime)+" ms");
             writer.close();
         } catch (IOException e) {
             e.printStackTrace();
         }
+
     }
 }
